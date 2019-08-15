d786cfc7c503178712a3640f691468f653c072e4
Timothy Shores <timothymshores@gmail.com>
2019-08-15 17:08:59 -0500

    Create remove_high function in making_changes.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,10 +3,28 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+
 def eating_cookies(n, cache=None):
-  pass
+  divide_by_3 = 0
+  divide_by_2 = 0
+  divide_by_1 = 0
+
+ if n < 2:
+     return 1
+ elif n == 1:
+     return 2
+ elif n == 3:
+     return 4
+ elif n == 4:
+     return 5
+    # eat 1 cookie 4 times
+    # eat 1 then eat 3
+    # eat 2 then 2
+    # eat 1 then 4
+    # eat 4
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,47 @@
 
 import sys
 
+
+def remove_high(value, denominations):
+    index = 0
+
+    for coin in denominations:
+        if coin <= amount:
+            index += 1
+
+    return denominations[:index]
+
+
 def making_change(amount, denominations):
-  pass 
+    count = 0
+    denominations = remove_high(amount, denominations)
+
+    if amount < 2:
+        return 1
+    else:
+        for denomination in denominations:
+            print(
+                f"Line 12: Count: {count}, Amount: {amount}, Demonination: {denomination}")
+            if amount % denomination == 0:
+                print(f"Amount: {amount} is   {denomination}")
+                count += 1
+            print(
+                f"Line 16: Count: {count}, Amount: {amount}, Demonination: {denomination}")
+            if amount > denomination and denomination > 1:
+                print(
+                    f"Line 19: Count: {count}, Amount: {amount}, Demonination: {denomination}")
+                making_change(amount // denomination, denominations)
+                count += 1
+    return count
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
